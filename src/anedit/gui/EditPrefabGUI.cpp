97aec2ede28f875743ecc7ce005d989b047f92e6
Anecoz <heilmair5@gmail.com>
2024-01-15 19:20:39 +0100

    Allow changing prefab transform in gui

--- src/anedit/gui/EditPrefabGUI.cpp
+++ src/anedit/gui/EditPrefabGUI.cpp
@@ -23,6 +23,8 @@ void EditPrefabGUI::immediateDraw(logic::AneditContext* c)
     return;
   }
 
+  bool changed = false;
+
   // Name
   char name[100];
   name[0] = '\0';
@@ -30,8 +32,39 @@ void EditPrefabGUI::immediateDraw(logic::AneditContext* c)
   strcpy_s(name, prefab->_name.c_str());
   ImGui::Text("Name");
   if (ImGui::InputText("##prefabname", name, 100)) {
+    changed = true;
+  }
+
+  // Transform
+  auto trans = prefab->_comps._trans._localTransform;
+  glm::vec3 translation{ 0.0f };
+  glm::vec3 scale{ 0.0f };
+  glm::vec3 rot{ 0.0f };
+  ImGuizmo::DecomposeMatrixToComponents(&trans[0][0], &translation[0], &rot[0], &scale[0]);
+
+  // Inputs for transform
+  ImGui::Text("Translation");
+  if (ImGui::InputFloat3("##prefabtranslation", &translation[0], "%.3f", ImGuiInputTextFlags_EnterReturnsTrue)) {
+    changed = true;
+  }
+
+  ImGui::Text("Scale");
+  if (ImGui::InputFloat3("##prefabscale", &scale[0], "%.3f", ImGuiInputTextFlags_EnterReturnsTrue)) {
+    changed = true;
+  }
+
+  ImGui::Text("Rotation");
+  if (ImGui::InputFloat3("##rprefabot", &rot[0], "%.3f", ImGuiInputTextFlags_EnterReturnsTrue)) {
+    changed = true;
+  }
+
+  if (changed) {
+    glm::mat4 m;
+    ImGuizmo::RecomposeMatrixFromComponents(&translation[0], &rot[0], &scale[0], &m[0][0]);
+
     auto copy = *prefab;
     copy._name = name;
+    copy._comps._trans._localTransform = m;
     c->scene().updatePrefab(std::move(copy));
   }
 }
